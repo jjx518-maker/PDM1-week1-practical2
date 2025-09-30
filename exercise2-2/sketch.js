d59c88301de7c3723f07a7cbe31a38fbc6a440a4
jjx518-maker <jjx518@york.ac.uk>
2025-09-30 21:17:10 +0100

    Update sketch.js

---
+++ exercise2-2/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(300, 300);
+    noStroke();
+}
+
+function draw() {
+    background(0);
+    fill(255);
+    rect(0,0,100,100);
+    rect(0,200,100,100);
+    rect(200,0,100,100);
+    rect(200,200,100,100);
+}
